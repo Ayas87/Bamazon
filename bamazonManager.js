439def219b64dfd325179b504c6c2e687dd43dbb
Randy <randylam87@gmail.com>
2017-06-11 19:04:34 -0700

    manager update stock partially working

--- bamazonManager.js
+++ bamazonManager.js
@@ -43,18 +43,30 @@ let start = () => {
             console.log('\n');
             console.table(result);
             start();
+            
         }
     };
-    
+
+    let selectStock = {
+        statement: `SELECT item_id ID, product_name Product, department_name Department, price Price, stock_quantity Stock FROM Products`,
+        callback: (result) => {
+            console.log('\033[2J');
+            productsObj = result;
+            console.log('\n');
+            console.table(result);
+        }
+    };
+
+
 
     inquirer.prompt([{
         name: 'menu',
         message: 'What would you like to do?',
         type: 'list',
-        choices: ['View Products for Sale','View Low Inventory','Add to Inventory', 'Add New Product']
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
     }]).then((result) => {
         console.log(result.menu);
-        switch(result.menu){
+        switch (result.menu) {
             case 'View Products for Sale':
                 query(viewProducts);
                 break;
@@ -62,7 +74,39 @@ let start = () => {
                 query(viewLowInventory);
                 break;
             case 'Add to Inventory':
-                //placeholder
+                query(selectStock)
+                    .then((result) => {
+                        inquirer.prompt([{
+                                name: 'productId',
+                                message: 'What Product ID would you like up add stock to?',
+                                type: 'input'
+                            },
+                            {
+                                name: 'stock',
+                                message: 'How many would you like to add?',
+                                type: 'input'
+                            }
+                        ]).then((result) => {
+                            let productId = result.productId - 1;
+                            let oldstock = productsObj[productId].Stock;
+                            let addstock = result.stock;
+                            console.log('old stock ' + parseInt(`${oldstock}`));
+                            let newStock = parseInt(`${oldstock}`) + parseInt(`${addstock}`);
+                            let addStock = {
+                                statement: `UPDATE Products SET stock_quantity = ${newStock} WHERE item_id = ${productId} `,
+                                callback: (result) => {
+                                    console.log('\033[2J');
+                                    productsObj = result;
+                                    console.log('\n');
+                                    console.table(result);
+                                }
+                            };
+                            // query(addStock);
+                            console.log(`newStock ${newStock}`);
+                            console.log(`productId ${result.productId}`)
+
+                        });
+                    });
                 break;
             case 'Add New Product':
                 //placeholder
