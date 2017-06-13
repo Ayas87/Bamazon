30fe4cfaae4c668981b2ec35cb38a2d5f5cc3d12
Ayas87 <randylam87@gmail.com>
2017-06-13 15:55:40 -0700

    completed manager, missing validation

--- bamazonManager.js
+++ bamazonManager.js
@@ -24,6 +24,7 @@ let start = () => {
         return queryPromise;
     };
 
+
     let viewProducts = {
         statement: `SELECT item_id ID, product_name Product, department_name Department, price Price, stock_quantity Stock FROM Products`,
         callback: (result) => {
@@ -43,7 +44,7 @@ let start = () => {
             console.log('\n');
             console.table(result);
             start();
-            
+
         }
     };
 
@@ -57,15 +58,13 @@ let start = () => {
         }
     };
 
-
-
     inquirer.prompt([{
         name: 'menu',
         message: 'What would you like to do?',
         type: 'list',
         choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
     }]).then((result) => {
-        console.log(result.menu);
+        // console.log(result.menu);
         switch (result.menu) {
             case 'View Products for Sale':
                 query(viewProducts);
@@ -87,29 +86,80 @@ let start = () => {
                                 type: 'input'
                             }
                         ]).then((result) => {
-                            let productId = result.productId - 1;
-                            let oldstock = productsObj[productId].Stock;
+                            let productId = result.productId;
+                            let oldstock = productsObj[productId - 1].Stock;
                             let addstock = result.stock;
                             console.log('old stock ' + parseInt(`${oldstock}`));
                             let newStock = parseInt(`${oldstock}`) + parseInt(`${addstock}`);
                             let addStock = {
-                                statement: `UPDATE Products SET stock_quantity = ${newStock} WHERE item_id = ${productId} `,
+                                statement: `UPDATE Products SET stock_quantity = ${newStock} WHERE item_id = ${productId}`,
                                 callback: (result) => {
                                     console.log('\033[2J');
-                                    productsObj = result;
                                     console.log('\n');
-                                    console.table(result);
+                                    console.log(`Current stock is ${newStock} of product ID ${productId}. \n`);
+                                    start();
                                 }
                             };
-                            // query(addStock);
-                            console.log(`newStock ${newStock}`);
-                            console.log(`productId ${result.productId}`)
-
+                            query(addStock);
                         });
                     });
                 break;
             case 'Add New Product':
-                //placeholder
+                let departmentNames = {
+                    statement: `SELECT department_name FROM Products`,
+                    callback: (result) => {
+                        console.log('\033[2J');
+                        console.log('\n');
+                        return result;
+                    }
+                };
+
+                query(departmentNames)
+                    .then((result) => {
+                        let departments = [];
+                        for (i = 0; i < result.length; i++) {
+                            if (departments.indexOf(result[i].department_name) == -1) {
+                                departments.push(result[i].department_name);
+                            }
+                        }
+                        inquirer.prompt([{
+                            name: 'department',
+                            message: 'What department do you want to add your product to?',
+                            type: 'list',
+                            choices: departments
+                        },
+                        {
+                            name: 'product',
+                            message: 'What is the name of the product do you want to add?',
+                            type: 'input',
+                        },
+                        {
+                            name: 'price',
+                            message: 'How much does each item cost?',
+                            type: 'input',
+                        },
+                        {
+                            name: 'stock',
+                            message: 'How many do you want to add?',
+                            type: 'input',
+                        },
+                        ]).then((result) => {
+                            let department = result.department;
+                            let product = result.product;
+                            let price = result.price;
+                            let stock = result.stock;
+                            console.log(product,department, price, stock);
+                            let addNewItem = {
+                                statement: `INSERT INTO Products (product_name, department_name, price ,stock_quantity) VALUES ('${product}', '${department}', ${price}, ${stock})`,
+                                // statement: 'SELECT * FROM Products',
+                                callback: (result)=>{
+                                    console.log(`${stock} ${product} was added to ${department} for ${price} per unit.`);
+                                    start();
+                                }
+                            };
+                            query(addNewItem);
+                        });
+                    });
                 break;
             default:
                 console.log('Invalid choice');
